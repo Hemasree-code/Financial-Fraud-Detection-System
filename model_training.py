bbcb6126317e88c0d76fad72b4064e577cad71ff
Hemasree-code <hemasree.r2022@vitstudent.ac.in>
2026-05-31 12:25:24 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,4 +1,15 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-print("Model Training Started")
+print("Financial Fraud Detection Model Training")
+
+# Load Dataset
+df = pd.read_csv("dataset/fraud.csv")
+
+print("Dataset Loaded Successfully")
+print(df.head())
+
+# Dummy model creation
+model = RandomForestClassifier()
+
+print("Model Created Successfully")
